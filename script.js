93f4c110078302a60aafc9759bd5b933733b6fb8
ahadakash <sadhinakash181@gmail.com>
2023-08-31 12:51:56 +0600

    second

---
+++ script.js
@@ -0,0 +1,8 @@
+// let data = prompt("Enter some data:", "Default data");
+
+// alert("Hello, World!");
+
+// document.write('Hello, World!');
+
+const num1 = parseInt(prompt('Enter the first number '));
+
